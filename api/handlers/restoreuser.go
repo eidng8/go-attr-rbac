efa97a1bf63be19a88f83c38ace637d64e95c914
eidng8 <jackey.cheung@qq.com>
2024-12-11 22:02:04 +0800

    do test

--- api/handlers/restoreuser.go
+++ api/handlers/restoreuser.go
@@ -2,24 +2,39 @@ package handlers
 
 import (
 	"context"
+	"net/http"
+
+	"github.com/eidng8/go-ent/softdelete"
 
 	"github.com/eidng8/go-attr-rbac/ent"
+	"github.com/eidng8/go-attr-rbac/ent/user"
 )
 
 // RestoreUser restores a user.
 //
 // Endpoint: POST /user/{id}/restore
 func (s Server) RestoreUser(
-	ctx context.Context, request RestoreUserRequestObject,
+	_ context.Context, request RestoreUserRequestObject,
 ) (RestoreUserResponseObject, error) {
+	t := true
 	_, err := s.db.Transaction(
-		context.Background(),
-		func(ctx context.Context, tx *ent.Tx) (interface{}, error) {
-			return nil, tx.User.UpdateOneID(request.Id).ClearDeletedAt().
-				Exec(ctx)
+		softdelete.NewSoftDeleteQueryContext(&t, context.Background()),
+		func(qc context.Context, tx *ent.Tx) (interface{}, error) {
+			return nil, tx.User.UpdateOneID(request.Id).
+				Where(user.DeletedAtNotNil()).ClearDeletedAt().Exec(qc)
 		},
 	)
 	if err != nil {
+		if ent.IsNotFound(err) {
+			var s interface{} = "not found"
+			return RestoreUser404JSONResponse{
+				N404JSONResponse: N404JSONResponse{
+					Code:   http.StatusNotFound,
+					Status: "error",
+					Errors: &s,
+				},
+			}, nil
+		}
 		return nil, err
 	}
 	return RestoreUser204Response{}, nil
